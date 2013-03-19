c7843d18969328951cd81851e549c95921d1bb74
Will Trimble <trimble@anl.gov>
2013-03-19 19:15:56 -0400

    More sensible color scheme, execptions on API

--- src/plot-kmer-spectrum.py
+++ src/plot-kmer-spectrum.py
@@ -5,6 +5,7 @@ import sys, os
 import numpy as np
 import matplotlib.pyplot as plt
 from optparse import OptionParser
+import time
 
 def getcolor(a):
     colorlist = ["b", "g", "r", "c", "y", "m", "k"] 
@@ -13,6 +14,8 @@ def getcolor(a):
 
 def cleanlabel(label):
     '''Sanitizes graph labels of unintersting file extensions'''
+    if label.find(".preprocess.kmerhistogram.txt") > 0:
+        label = label[0:(label.find(".preprocess.kmerhistogram.txt"))]
     if label.find(".histhist") > 0:
         label = label[0:(label.find(".histhist"))]
     if label.find(".fastq") > 0:
@@ -22,7 +25,7 @@ def cleanlabel(label):
     if label.find(".csv") > 0:
         label = label[0:(label.find(".csv"))]
     return label
- 
+
 def getmgrkmerspectrum(accessionnumber):
     '''Retrieve kmer spectrum from MG-RAST'''
     import urllib
@@ -31,8 +34,12 @@ def getmgrkmerspectrum(accessionnumber):
     some_url = "http://api.metagenomics.anl.gov/api2.cgi/metagenome_statistics/%s?verbosity=full" % accessionnumber
     if key != None:
         some_url = some_url+"&auth=%s" % key
-    sys.stderr.write(some_url+"\n")
-    jsonobject = urllib.urlopen(some_url).read()
+    sys.stderr.write("Sending request for "+some_url+"\n")
+    time.sleep(1)
+    try: 
+        jsonobject = urllib.urlopen(some_url).read()
+    except: 
+        sys.stderr.write("Error retrieving %s"%some_url) 
     j = json.loads(jsonobject)
     try: 
         sys.stderr.write("Error with %s\nERROR : %s\n"%(some_url, j["ERROR"]))
@@ -55,13 +62,18 @@ def makegraphs(a, filename, option=6, label=None, n=0):
     (b_cn, b_c1, b_yd, b_yo, b_zd, b_zo, b_y) = calccumsum(b)
     (c_cn, c_c1, c_yd, c_yo, c_zd, c_zo, c_y) = calccumsum(c)  
     x = np.arange(len(b[:, 0]))                  # rank
-    color=getcolor(n)
+    color = getcolor(n)
     if option == 0:
         pA = plt.loglog(cn, c1, "-",  color = color, label=tracelabel)
         pA = plt.loglog(cn, c1, ".",  color = color)
         plt.xlabel("kmer coverage")
         plt.ylabel("number of kmers")
         plt.legend(loc="upper right")
+    if option == 0 or option == -1:
+        if opts.dump:
+            c = np.hstack((cn.reshape((len(cn), 1)), (c1.reshape((len(cn), 1))  )  ))
+            sys.stderr.write("saving output table in %s.0.plot.csv\n" % filename) 
+            np.savetxt("%s.0.plot.csv" % filename,  c, fmt = ['%d', '%d'] , delimiter="\t" )
     elif option == 1:
         pA = plt.loglog(cn, cn*c1, "-", color=color, label=tracelabel )
         pA = plt.loglog(cn, cn*c1, '.', color=color)
@@ -69,6 +81,10 @@ def makegraphs(a, filename, option=6, label=None, n=0):
         plt.ylabel("kmers observed")
         plt.legend(loc="upper right")
         plt.grid(1)
+        if opts.dump:
+            c = np.hstack((cn.reshape((len(cn), 1)), ((cn*c1).reshape((len(cn), 1))  )  ))
+            sys.stderr.write("saving output table in %s.1.plot.csv\n" % filename) 
+            np.savetxt("%s.1.plot.csv" % filename,  c, fmt = ['%d', '%d'] , delimiter="\t" )
     elif option == 2:
         pA = plt.loglog(yo, cn, label=tracelabel )
         plt.xlabel("observed kmers ")
@@ -93,8 +109,8 @@ def makegraphs(a, filename, option=6, label=None, n=0):
         plt.grid(1)
     elif option == 5: 
         z = zo/ zo.max() 
-        pA = plt.semilogx( yd, z )
-        pA = plt.semilogx( yd, z, '.', label=tracelabel )
+        pA = plt.semilogx( yd, z , '-', color=color )
+        pA = plt.semilogx( yd, z, '.', color=color, label=tracelabel )
         plt.xlabel("kmer rank")
         plt.ylabel("fraction of observed kmers")
         plt.xlim((1, 10**9))
@@ -103,52 +119,54 @@ def makegraphs(a, filename, option=6, label=None, n=0):
         plt.legend(loc="lower left")
     elif option == 6:
         z = zo/ zo.max() 
-        pA = plt.loglog( yd, cn, '-', label=tracelabel)
+        pA = plt.loglog( yd, cn, '-', color=color, label=tracelabel)
+        pA = plt.loglog( yd, cn, '.', color=color )
         plt.xlabel("kmer rank")
         plt.ylabel("kmer coverage ")
         plt.xlim((1, 10**8))
         plt.ylim(1, 10**7)
         plt.grid(1)
         plt.legend(loc="lower left")
+        if opts.dump:
+            c = np.hstack((yd.reshape((len(yd), 1)), cn.reshape((len(cn), 1))  )  )
+            sys.stderr.write("saving output table in %s.6.plot.csv\n" % filename) 
+            np.savetxt("%s.6.plot.csv" % filename,  c, fmt = ['%d', '%d'] , delimiter="\t" )
     elif option == 7:
-        pA = plt.plot( x, c_zd, '.-', label=tracelabel)
+        pA = plt.plot( x, c_zd, '.-', color=color, label=tracelabel)
         plt.xlabel("contig size rank")
         plt.ylabel("cuml contig size (bp) ")
         plt.grid(1)
         plt.legend(loc="upper right") 
     elif option == 8:
-        pA = plt.plot( x, c_zo/max(c_zo) , '.-', label=tracelabel)
+        pA = plt.plot( x, c_zo/max(c_zo) , '.-', color=color, label=tracelabel)
         plt.xlabel("contig size rank ")
         plt.ylabel("frac data explained ")
         plt.grid(1)
         plt.legend(loc="upper right") 
     elif option == 9:
-        pA = plt.plot( x, c_zo/max(c_zo) , '.-', label=tracelabel)
+        pA = plt.plot( x, c_zo/max(c_zo) , '-', color=color, label=tracelabel)
         plt.xlabel("contig size rank ")
         plt.ylabel("frac data explained ")
         plt.grid(1)
         plt.legend(loc="upper right")
     elif option == 10:
-        pA = plt.plot( x, b_zo/max(b_zo) , '.-', label=tracelabel)
+        pA = plt.plot( x, b_zo/max(b_zo) , '.-', color=color, label=tracelabel)
         plt.xlabel("contig cov rank ")
         plt.ylabel("frac data explained ")
         plt.grid(1)
         plt.legend(loc="upper right")
     elif option == 11:
-        pA = plt.plot( x, b_zo , '.-', label=tracelabel)
+        pA = plt.plot( x, b_zo , '.-', color=color,  label=tracelabel)
         plt.xlabel("contig cov rank ")
         plt.ylabel("data explained (bogo bp) ")
         plt.grid(1)
         plt.legend(loc="upper right")
     elif option == 12:
-        pA = plt.plot( c_zd , c_zo , '.-', label=tracelabel)
+        pA = plt.plot( c_zd , c_zo , '.-', color=color, label=tracelabel)
         plt.xlabel("contig cov rank ")
         plt.ylabel("data explained (bogo bp) ")
         plt.grid(1)
         plt.legend(loc="upper right")
-    if opts.dump:
-        c = np.hstack((yd.reshape((len(yd), 1)), cn.reshape((len(cn), 1))  )  )
-        np.savetxt("%s.plot.csv" % filename,  c, fmt = ['%d', '%.1f'] , delimiter="\t" )
 
 def calccumsum(a):
     '''Calcaulates the cumulative-sum vectors from a 2d numpy array of [cov, num].  Note depends on upstream sort '''  
@@ -168,7 +186,6 @@ def printstats(a, filename, filehandle=None, n=0):
     T  = zo.max()
     y  = zo/ T
     j  = cn / T
-    fk = c1 / T
     H = 10**(sum(- c1 * j * np.log(j) /np.log(10)))  # Entropy
     H2 = 1  /(sum( c1*j* j ))                         # Reyni entropy
     M90 = yd[np.nonzero(y > 0.1)[0]][0]
@@ -209,11 +226,13 @@ def loadfile(filename):
 def main(filename, option=6, label=None, n=0 ):
     '''Main driver; loads file and invokes makegraphs and printstats 
     to append graphics from each file onto the figure'''
-    N=0
-    if opts.filetype == "mgr":
+    logfh = open(opts.logfile, "a")
+    if opts.filetype.upper() == "MGM":
         a = getmgrkmerspectrum(filename)
-    else:
+    elif opts.filetype == "file":
         a = loadfile(filename)
+    else: 
+        raise ValueError("%s is invalid type (valid types are mgm and file)"%opts.filetype ) 
     if label == None:
         label = filename
     if a != None:
@@ -221,8 +240,8 @@ def main(filename, option=6, label=None, n=0 ):
         sys.stderr.write("Making graphs for %s\n" % filename)
         makegraphs(a, filename, option, label, n=n )
         try: 
-            sys.stderr.write("Printing stats in %s.csv %d\n" % (filename, n))
-            printstats(a, "%s.csv" % filename)
+            sys.stderr.write("Printing stats in logfile %s %d\n" % (opts.logfile, n))
+            printstats(a, filename, filehandle=logfh, n=n )
             printstats(a, filename, filehandle=sys.stdout, n=n) 
             n += 1
         except: 
@@ -247,7 +266,9 @@ if __name__ == '__main__':
     parser.add_option("-l", "--list", dest="filelist",  
          default=None, help="file containing list of targets and labels")
     parser.add_option("-t", "--type", dest="filetype", 
-         default="file", help="type for file list (file,mgr)")
+         default="file", help="type for file list (file,mgm)")
+    parser.add_option("-a", "--appendlogfile", dest="logfile", 
+         default="kmers.log", help="logfile for summary statistics")
   
     (opts, args) = parser.parse_args()
     option = opts.option
@@ -256,14 +277,14 @@ if __name__ == '__main__':
     else: 
         imagefilename = "out.%d.pdf" % (option,)
         sys.stderr.write("Warning, using default filename %s\n" % (imagefilename,))
-    if opts.filetype == "mgr":  
+    if opts.filetype == "mgm":  
         try:
             key = os.environ["MGRKEY"]
         except KeyError:
             key = ""
 
     graphcount = 0
-    if opts.filelist and (opts.filetype == "file" or opts.filetype == "mgr"):
+    if opts.filelist: 
         assert os.path.isfile(opts.filelist), "File %s does not exist"%opts.filelist
         IN_FILE = open(opts.filelist, "r") 
         for line in IN_FILE:
@@ -276,8 +297,9 @@ if __name__ == '__main__':
         for f in args :
             filen = f
             graphcount = main(filen, option, n=graphcount)
-    sys.stderr.write("Writing graph into file %s\n" % (imagefilename))
-    plt.savefig(imagefilename)
+    if option != -1:
+        sys.stderr.write("Writing graph into file %s\n" % (imagefilename))
+        plt.savefig(imagefilename)
     if opts.interactive:
         plt.show()
     else:
