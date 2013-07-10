2ec871fcbe4df8b7ba7eff840cfb25f372969008
Will Trimble <trimble@anl.gov>
2013-07-10 16:38:35 -0500

    Bug in initial guess--trimmed twice

--- test/calculatit.sh
+++ test/calculatit.sh
@@ -1,6 +1,17 @@
 
 
 cd total
-for i in *.fastq; do countkmer21.sh $i; done 
+for i in *.fastq; do echo $i; countkmer21.sh $i; done 
+
+mkdir fits
+cd fits
+ln ../total/*.21 .
 
 
+for i in *.21 
+do 
+if [[ ! -e $i.fit.png ]] 
+then
+kmerspectrumanalyzer.py -q -n 30 $i 
+fi 
+done 
