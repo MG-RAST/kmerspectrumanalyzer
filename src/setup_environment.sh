79da2b3244c9ff24a0a668cd7ce33daa6595d5b9
Will Trimble <trimble@anl.gov>
2013-08-19 07:49:01 -0500

    Corected punctuation error in setup

--- src/setup_environment.sh
+++ src/setup_environment.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y git
 git clone http://github.com/wltrimbl/kmerspectrumanalyzer.git   # The development versison
 # git clone http://github.com/MG-RAST/kmerspectrumanalyzer.git 
 
-echo "export PATH=$PATH:~/kmerspectrumanalyzer/src" >> ~/.bash_profile 
+echo export PATH=\$PATH:~/kmerspectrumanalyzer/src >> ~/.bash_profile 
 source ~/.bash_profile 
 
 # The ubuntu jellyfish package is jellyfish 1.1.6 , probably only changes are bugfixes.  
