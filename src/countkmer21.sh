113e2d723b0a9710062f2e7470c315874bbde15d
Will Trimble <trimble@anl.gov>
2013-08-19 08:15:00 -0500

    Added more error handling to counkmer15.sh

--- src/countkmer21.sh
+++ src/countkmer21.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 filename=$1
 k=21
-kmer-tool2  -t fastq -l $k -f histo -i $filename -o $filename.${k}
+filetype=fastq
+USAGE="Usage: countkmer${k}.sh <fastq filename>"
+if [ $# -lt 1 ]
+    then
+    echo "Error: fastq filename is required"
+    echo $USAGE 
+    exit
+elif [ $# -gt 1 ]
+    then 
+    echo "Error: only one argument is required"
+    echo $USAGE 
+    exit
+elif [[ ! -e $filename ]] 
+    then 
+    echo "Error: Input filename $filename does not exist."
+    echo $USAGE 
+    exit
+fi 
+echo "Counting ${k}mers in $filename, creating $filename.${k} with counts."
+kmer-tool2  -t $filetype -l $k -f histo -i $filename -o $filename.${k}
