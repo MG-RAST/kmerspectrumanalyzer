7ebbcfda1adba7aa03d5f9d73a0c04860a0c2c67
Will Trimble <trimble@anl.gov>
2013-11-21 16:32:43 -0600

    Added new testing cases

--- test/test-plotkmerspectrumanalyzer.sh
+++ test/test-plotkmerspectrumanalyzer.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 rm kmers.log
 
+plot-kmer-spectrum.py -g -1  test0  # only one count, H = H2 = 1
 plot-kmer-spectrum.py -g -1  test1
 plot-kmer-spectrum.py -g -1  test2
-plot-kmer-spectrum.py -g -1  test3
+plot-kmer-spectrum.py -g -1  test3  # uniform distribution  H = H2 = 1000
 plot-kmer-spectrum.py -g -1  test4
 plot-kmer-spectrum.py -g -1  test5
-plot-kmer-spectrum.py -g -1  test6
-plot-kmer-spectrum.py -g -1  test7
-plot-kmer-spectrum.py -g -1  test8
+plot-kmer-spectrum.py -g -1  test6  # all at the same abundance
+plot-kmer-spectrum.py -g -1  test7  # all at the same abundance 
+plot-kmer-spectrum.py -g -1  test8  # all at the same abundance
 plot-kmer-spectrum.py -g -1  test9
-plot-kmer-spectrum.py -g -1  test10
-plot-kmer-spectrum.py -g -1  testF1
-plot-kmer-spectrum.py -g -1  testF2
-plot-kmer-spectrum.py -g -1  testF3
-plot-kmer-spectrum.py -g -1  test11
+plot-kmer-spectrum.py -g -1  test10  # rows out of order
+plot-kmer-spectrum.py -g -1  testF1  # too many zeros
+plot-kmer-spectrum.py -g -1  testF2  # zero in the 2nd column
+plot-kmer-spectrum.py -g -1  testF3  # zero in the 1st column
+plot-kmer-spectrum.py -g -1  testF4  # empty file
+plot-kmer-spectrum.py -g -1  test11  # zero in the 1st column
+plot-kmer-spectrum.py -g -1  test12  # zero in the 2nd column
