5999a9d02839bfaf9964af596194bb2b393d2716
Will Trimble <trimble@anl.gov>
2014-01-22 14:53:33 -0600

    Changed simple test case filenames

--- test/test-plotkmerspectrumanalyzer.sh
+++ test/test-plotkmerspectrumanalyzer.sh
@@ -3,20 +3,20 @@
 
 rm kmers.log
 
-plot-kmer-spectrum.py -g -1  test0  # only one count, H = H2 = 1
-plot-kmer-spectrum.py -g -1  test1
-plot-kmer-spectrum.py -g -1  test2
-plot-kmer-spectrum.py -g -1  test3  # uniform distribution  H = H2 = 1000
-plot-kmer-spectrum.py -g -1  test4
-plot-kmer-spectrum.py -g -1  test5
-plot-kmer-spectrum.py -g -1  test6  # all at the same abundance
-plot-kmer-spectrum.py -g -1  test7  # all at the same abundance 
-plot-kmer-spectrum.py -g -1  test8  # all at the same abundance
-plot-kmer-spectrum.py -g -1  test9
-plot-kmer-spectrum.py -g -1  test10  # rows out of order
-plot-kmer-spectrum.py -g -1  testF1  # too many zeros
-plot-kmer-spectrum.py -g -1  testF2  # zero in the 2nd column
-plot-kmer-spectrum.py -g -1  testF3  # zero in the 1st column
-plot-kmer-spectrum.py -g -1  testF4  # empty file
-plot-kmer-spectrum.py -g -1  test11  # zero in the 1st column
-plot-kmer-spectrum.py -g -1  test12  # zero in the 2nd column
+plot-kmer-spectrum.py -g -1  test00.21  # only one count, H = H2 = 1
+plot-kmer-spectrum.py -g -1  test01.21
+plot-kmer-spectrum.py -g -1  test02.21
+plot-kmer-spectrum.py -g -1  test03.21  # uniform distribution  H = H2 = 1000
+plot-kmer-spectrum.py -g -1  test04.21
+plot-kmer-spectrum.py -g -1  test05.21
+plot-kmer-spectrum.py -g -1  test06.21  # all at the same abundance
+plot-kmer-spectrum.py -g -1  test07.21  # all at the same abundance 
+plot-kmer-spectrum.py -g -1  test08.21  # all at the same abundance
+plot-kmer-spectrum.py -g -1  test09.21
+plot-kmer-spectrum.py -g -1  test10.21  # rows out of order
+plot-kmer-spectrum.py -g -1  test11.21  # zero in the 1st column
+plot-kmer-spectrum.py -g -1  test12.21  # zero in the 2nd column
+plot-kmer-spectrum.py -g -1  testF1.21  # too many zeros
+plot-kmer-spectrum.py -g -1  testF2.21  # zero in the 2nd column
+plot-kmer-spectrum.py -g -1  testF3.21  # zero in the 1st column
+plot-kmer-spectrum.py -g -1  testF4.21  # empty file
