383ad8e74d8eec5bd63760a78a54a30d2db224ef
Will Trimble <trimble@anl.gov>
2014-02-11 12:00:53 -0600

    whitesapce cleanup

--- src/test_ksa.py
+++ src/test_ksa.py
@@ -8,12 +8,12 @@ import numpy as np
 from numpy import array
 
 def test_calccumsum_pass():
-    data = np.array( [[2000, 1000000] , [20000, 10000], [200000, 100], [1000000, 1], [2000000, 1] ], dtype="float")
+    data = np.array([[2000, 1000000], [20000, 10000], [200000, 100], [1000000, 1], [2000000, 1]], dtype="float")
     cn, c1, yd, yo, zd, zo = calccumsum(data)
     pass
 
 def test_calccumsum_00_file():
-    data = loadfile("../test/test00.21") 
+    data = loadfile("../test/test00.21")
     cn, c1, yd, yo, zd, zo = calccumsum(data)
     exp = 0
     assert_equal(cn, 1)
@@ -24,31 +24,31 @@ def test_calccumsum_00_file():
     assert_equal(zo, 1)
 
 def test_calccumsum_01_array_int():
-    data = np.array( [ [1,910], [2,45] ], dtype="int")
+    data = np.array([[1, 910], [2, 45]], dtype="int")
     cn, c1, yd, yo, zd, zo = calccumsum(data)
-    assert_true(np.all(cn ==  array([1, 2])) )
-    assert_true(np.all(c1 == array([910,  45])))
-    assert_true(np.all(yd == array([955,  45])))
-    assert_true(np.all(yo == array([1000,  90])))
-    assert_true(np.all(zd == array([910, 955]) ))
+    assert_true(np.all(cn == array([1, 2])))
+    assert_true(np.all(c1 == array([910, 45])))
+    assert_true(np.all(yd == array([955, 45])))
+    assert_true(np.all(yo == array([1000, 90])))
+    assert_true(np.all(zd == array([910, 955])))
     assert_true(np.all(zo == array([910, 1000])))
 
 def test_calccumsum_01_array_float():
-    data = np.array( [ [1,910], [2,45] ], dtype="float")
+    data = np.array([[1, 910], [2, 45]], dtype="float")
     cn, c1, yd, yo, zd, zo = calccumsum(data)
-    assert_true(np.all(cn ==  array([1, 2])) )
-    assert_true(np.all(c1 ==  array([910,  45])))
-    assert_true(np.all(yd == array([955,  45])))
-    assert_true(np.all(yo == array([1000,  90])))
-    assert_true(np.all(zd == array([910, 955]) ))
+    assert_true(np.all(cn == array([1, 2])))
+    assert_true(np.all(c1 == array([910, 45])))
+    assert_true(np.all(yd == array([955, 45])))
+    assert_true(np.all(yo == array([1000, 90])))
+    assert_true(np.all(zd == array([910, 955])))
     assert_true(np.all(zo == array([910, 1000])))
 
 def test_calccumsum_int():
-    data = np.array( [[2000, 1000000] , [20000, 10000], [200000, 100], [1000000, 1], [2000000, 1] ], dtype="int")
+    data = np.array([[2000, 1000000], [20000, 10000], [200000, 100], [1000000, 1], [2000000, 1]], dtype="int")
     cn, c1, yd, yo, zd, zo = calccumsum(data)
     pass
 
 def test_calccumsum_float():
-    data = np.array( [[2000, 1000000] , [20000, 10000], [200000, 100], [1000000, 1], [2000000, 1] ], dtype="float")
+    data = np.array([[2000, 1000000], [20000, 10000], [200000, 100], [1000000, 1], [2000000, 1]], dtype="float")
     cn, c1, yd, yo, zd, zo = calccumsum(data)
     pass
--- src/test_rare.py
+++ src/test_rare.py
@@ -8,34 +8,34 @@ from rare import fract
 
 # fract(aa, epsilon, threshold)
 def test_calccumsum_one_full():
-    data = np.array( [[100, 1000] ], dtype="float" )
-    result = fract(data, 1, 1) 
-    assert_equal(result , 1)
+    data = np.array([[100, 1000]], dtype="float")
+    result = fract(data, 1, 1)
+    assert_equal(result, 1)
 
 def test_calccumsum_one_zero():
-    data = np.array( [[100, 1000] ], dtype="float" )
-    result = fract(data, 1, 100000) 
-    assert_equal(result , 0)
+    data = np.array([[100, 1000]], dtype="float")
+    result = fract(data, 1, 100000)
+    assert_equal(result, 0)
 
 def test_calccumsum_one_half():
-    data = np.array( [[100, 1000] ], dtype="float" )
-    result = fract(data, 1, 100) 
+    data = np.array([[100, 1000]], dtype="float")
+    result = fract(data, 1, 100)
     assert_true(result > 0.3)
     assert_true(result < 0.7)
 
 def test_calccumsum_two_full():
-    data = np.array( [[100, 1000] , [20000, 10000] ], dtype="float" )
-    result = fract(data, 1, 1) 
+    data = np.array([[100, 1000], [20000, 10000]], dtype="float")
+    result = fract(data, 1, 1)
     assert_equal(result, 1)
- 
+
 def test_calccumsum_two_zero():
-    data = np.array( [[100, 1000] , [20000, 10000] ], dtype="float" )
-    result = fract(data, 1, 100000) 
+    data = np.array([[100, 1000], [20000, 10000]], dtype="float")
+    result = fract(data, 1, 100000)
     assert_equal(result, 0)
 
 def test_calccumsum_two_tenth():
-    data = np.array( [[100, 1000] , [1000, 1000] ], dtype="float" )
-    result = fract(data, 1, 200) 
+    data = np.array([[100, 1000], [1000, 1000]], dtype="float")
+    result = fract(data, 1, 200)
     assert_true(result > 0.88)
-    assert_true(result < 0.92) 
+    assert_true(result < 0.92)
 
