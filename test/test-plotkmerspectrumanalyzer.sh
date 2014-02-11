cf1def2f39c88d1c88e010771ed2c71e3d4c1009
Will Trimble <trimble@anl.gov>
2014-02-11 09:11:37 -0600

    Added file-diff tests to plotting tests

--- test/test-plotkmerspectrumanalyzer.sh
+++ test/test-plotkmerspectrumanalyzer.sh
@@ -16,7 +16,17 @@ plot-kmer-spectrum.py -g -1  test09.21
 plot-kmer-spectrum.py -g -1  test10.21  # rows out of order
 plot-kmer-spectrum.py -g -1  test11.21  # zero in the 1st column
 plot-kmer-spectrum.py -g -1  test12.21  # zero in the 2nd column
+# These should fail to produce any output
 plot-kmer-spectrum.py -g -1  testF1.21  # too many zeros
 plot-kmer-spectrum.py -g -1  testF2.21  # zero in the 2nd column
 plot-kmer-spectrum.py -g -1  testF3.21  # zero in the 1st column
 plot-kmer-spectrum.py -g -1  testF4.21  # empty file
+
+if [[ -z $( diff kmers.log kmers-1.log) ]]
+then
+echo Results match expectation
+else
+echo kmers.log differs from expectation in kmers-1.log
+fi
+
+
--- test/test-plotkmerspectrumanalyzer2.sh
+++ test/test-plotkmerspectrumanalyzer2.sh
@@ -11,6 +11,14 @@ plot-kmer-spectrum.py -l testlist4
 plot-kmer-spectrum.py -l testlist5
 plot-kmer-spectrum.py -l testlist6
 plot-kmer-spectrum.py -l testlist7
+plot-kmer-spectrum.py -l testlist8
 plot-kmer-spectrum.py .boguswc*
 plot-kmer-spectrum.py emptyfile 
 plot-kmer-spectrum.py -l mgrlist -t mgm
+
+if [[ -z $( diff kmers.log kmers-2.log) ]]
+then
+echo Results match expectation
+else
+echo kmers.log differs from expectation in kmers-2.log
+fi 
