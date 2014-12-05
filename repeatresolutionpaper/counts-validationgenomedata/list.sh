d603e9c967f2be4273e3148a5ba0b3f8b4392ca3
Will Trimble <trimble@anl.gov>
2014-12-05 15:48:38 -0600

    added data index to validation genomes

---
+++ repeatresolutionpaper/counts-validationgenomedata/list.sh
@@ -0,0 +1,7 @@
+
+stratify.py -l list  -g 0 -o list.frac3.pdf
+stratify.py -l list  -g 1 -o list.size3.pdf
+
+stratify.py -l list  -g 0 -s -o list.frac3s.pdf
+stratify.py -l list  -g 1 -s -o list.size3s.pdf
+
