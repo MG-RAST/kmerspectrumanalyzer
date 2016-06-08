a68094ebfe4007df952c924119427abd0ccfb913
Will Trimble <trimble@anl.gov>
2016-06-08 09:14:22 -0500

    added tests

--- src/test_ksa.py
+++ src/test_ksa.py
@@ -2,7 +2,9 @@
 from nose.tools import assert_equal, assert_almost_equal, assert_true, \
     assert_false, assert_raises, assert_is_instance
 
-from ksatools import calccumsum, loadfile
+from subprocess import call
+
+from ksatools import calccumsum, loadfile, renyispectrum, pad, smoothspectrum, calcmedian, cleanlabel, getmgrkmerspectrum, printstats, printstratify, makegraphs
 
 import numpy as np
 from numpy import array
@@ -56,3 +58,70 @@ def test_calccumsum_float():
     data = FIXTURE1
     cn, c1, yd, yo, zd, zo = calccumsum(data)
     pass
+
+def test_reyni_returns():
+    exponents = np.arange(.01, 5, .01)
+    renyi_sepctrum = renyispectrum(exponents, FIXTURE1)
+
+def test_pad():
+    xpadded, ypadded = pad(FIXTURE1[:,0], FIXTURE1[:,1])
+    assert_true(len(xpadded) > max(FIXTURE1[:,0]) * 1.2)
+    assert_true(len(ypadded) > max(FIXTURE1[:,0]) * 1.2)
+    assert_true(sum(ypadded) == sum(FIXTURE1[:,1]))
+
+def test_smooth():
+    s = smoothspectrum(FIXTURE1)
+
+def test_calcmedian():
+    yd = np.array([1,2,3,4])
+    y = np.array([10,20,30,40])
+    num = 35
+    s = calcmedian( yd, y, num )
+    print s
+    assert_true(s == 3.5)
+
+def test_cleanlabel():
+    assert_true( cleanlabel("file.histhist") == "file")
+    assert_true( cleanlabel("file.fastq") == "file")
+    assert_true( cleanlabel("file_info_contigstats.txt") == "file")
+    assert_true( cleanlabel("file.stats.txt") == "file")
+    assert_true( cleanlabel("file.txt") == "file")
+    assert_true( cleanlabel("file.csv") == "file")
+    assert_true( cleanlabel("file.037.kmerhistogram") == "file")
+
+def test_getmgrkmerspectrum():
+    g = getmgrkmerspectrum("mgm4441680.3")
+    assert_true( g.shape == (55,2))
+
+def test_getmgrkmerspectrum_fail():
+    g = getmgrkmerspectrum("mgm4441680.3", mgrkey="INVALID")
+
+def test_printstats():
+    p = printstats(FIXTURE1, "out.txt")
+
+def test_printstratify():
+    p = printstratify(FIXTURE1)
+
+def test_makesgraphs():
+    for i in range(-3,27):
+        print "testing visualization {:d}".format(i)
+        p = makegraphs(FIXTURE1, "filename", option = i)
+
+def test_makegraphs_d():
+    p = makegraphs(FIXTURE1, "filename", option=6, dump=True)
+
+def test_makegraphs_l():
+    p = makegraphs(FIXTURE1, "filename", option=6, label="ridiculouslabel")
+
+def test_makegraphs_30():
+    p = makegraphs(FIXTURE1, "filename", option=30)
+
+
+def test_cmdline_1():
+    call(["plotkmerspectrum.py", "-g", "-1", "../test00.21"])
+
+def test_cmdline_2():
+    call(["plotkmerspectrum.py", "-g", "0", "../test00.21"])
+
+#def test_cmdline_getmgr():
+#    call(['plotkmerspectrum.py','-l','../test/mgrlist','-i','-g','6','-t','mgm'])
--- src/test_rare.py
+++ src/test_rare.py
@@ -4,13 +4,19 @@ from nose.tools import assert_equal, assert_almost_equal, assert_true, \
 import numpy as np
 from numpy import array
 
-from rare import fract
+from rare import fract, calc_resampled_fraction, plotme
+
+FIXTURE = np.array([[100, 1000]], dtype="float")
+FIXTURE2 = np.array([[1,1000], [10,1000], [100, 1000]], dtype="float")
 
 # fract(aa, epsilon, threshold)
 def test_calccumsum_one_full():
     data = np.array([[100, 1000]], dtype="float")
     result = fract(data, 1, 1)
     assert_equal(result, 1)
+    result2 = fract(FIXTURE2, 1, 1)
+    assert_equal(result, 1)
+    result3 = fract(FIXTURE2, .5, 1)
 
 def test_calccumsum_one_zero():
     data = np.array([[100, 1000]], dtype="float")
@@ -40,3 +46,36 @@ def test_calccumsum_two_tenth():
     assert_true(result > 0.88)
     assert_true(result < 0.92)
 
+def test_calc_resampled_fraction():
+    samplefracs = np.array([[.01, .03, .1, .3]]).T
+    thresholds = np.array([[1,3,10,30,100]]).T
+    m = calc_resampled_fraction(FIXTURE, samplefracs, thresholds)
+    print m.shape 
+    assert_true(m.shape[0] == len(samplefracs))
+    assert_true(m.shape[1] == len(thresholds))
+    m2 = calc_resampled_fraction(FIXTURE2, samplefracs, thresholds)
+    assert_true(m2.shape[0] == len(samplefracs))
+    assert_true(m2.shape[1] == len(thresholds))
+
+def test_plotme0():
+    p = plotme(FIXTURE, "fixture label", shaded=0)
+    p = plotme(FIXTURE2, "fixture label", shaded=0)
+def test_plotme1():
+    p = plotme(FIXTURE, "fixture label", shaded=1)
+    p = plotme(FIXTURE2, "fixture label", shaded=1)
+def test_plotme2():
+    p = plotme(FIXTURE, "fixture label", shaded=2)
+    p = plotme(FIXTURE2, "fixture label", shaded=2)
+def test_plotme3():
+    p = plotme(FIXTURE, "fixture label", shaded=3)
+    p = plotme(FIXTURE2, "fixture label", shaded=3)
+def test_plotmeS():
+    p = plotme(FIXTURE, "fixture label", shaded=2, suppress=True)
+    p = plotme(FIXTURE2, "fixture label", shaded=2, suppress=True)
+def test_plotmeD():
+    p = plotme(FIXTURE, "fixture label", shaded=2, dump=True)
+    p = plotme(FIXTURE2, "fixture label", shaded=2, dump=True)
+
+def test_plotmeT():
+    p = plotme(FIXTURE, "fixture label", shaded=2, thresholdlist=[1,10,100])
+    p = plotme(FIXTURE2, "fixture label", shaded=2, thresholdlist=[1,10,100])
