69aa95e8d6219d7af542c82fb2694cf5ff8683d9
Will Trimble <trimble@anl.gov>
2017-01-19 10:52:26 -0600

    Added dependencies on make and curl (for example testing)

--- src/setup_environment.sh
+++ src/setup_environment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # This script should be sufficient to install the dependencies on a blank EC2 node
 
-#sudo apt-get update
-sudo apt-get install -y git 
+sudo apt-get update && sudo apt-get install -y git make curl
 git clone http://github.com/wltrimbl/kmerspectrumanalyzer.git   # The development versison
-# git clone http://github.com/MG-RAST/kmerspectrumanalyzer.git 
+# git clone http://github.com/MG-RAST/kmerspectrumanalyzer.git
 
-echo export PATH=\$PATH:~/kmerspectrumanalyzer/src >> ~/.bash_profile 
-source ~/.bash_profile 
+echo export PATH=\$PATH:~/kmerspectrumanalyzer/src >> ~/.bash_profile
+source ~/.bash_profile
 
-# The ubuntu jellyfish package is jellyfish 1.1.6 , probably only changes are bugfixes.  
+# The ubuntu jellyfish package is jellyfish 1.1.10
+sudo apt-get install -y jellyfish
 # matplotlib needed for visualizations
 # scipy needed for fitting
-sudo apt-get install -y jellyfish python-matplotlib python-scipy
+sudo pat-get install -y python-matplotlib python-scipy
 
 
